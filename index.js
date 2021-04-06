2d665ac8f499c172578e1abfbf2dbaf83a8bee1e
Tim <acronym1986@gmail.com>
2021-04-06 13:46:12 +1000

    code created and passed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// test 1 
+var customerName = 'bob';
+
+//test 2
+
+function upperCaseCustomerName () {
+   return customerName = customerName.toUpperCase();
+}
+
+
+//test 3 
+function setBestCustomer() {
+   bestCustomer = 'not bob' ;   
+}
+
+//test 4
+function  overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+//test  5
+const leastFavoriteCustomer = 'tim';
+function  changeLeastFavoriteCustomer() {
+       leastFavoriteCustomer = leastFavoriteCustomer;
+}
